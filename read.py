90e4bbe94431927a27245aa84e96e2ae9a621057
chuang-tw <chuang.15407@yahoo.com.tw>
2020-02-27 00:47:52 +0800

    version4

--- read.py
+++ read.py
@@ -19,3 +19,9 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print('一共有', len(new), '比留言長度小於100')
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+
